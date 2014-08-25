5382564f28a1e36dddfe61cbc58ae19b4e836c14
satya5561 <satya5561@gmail.com>
2014-08-25 13:28:13 +0530

    update 047

--- js/controllers.js
+++ js/controllers.js
@@ -768,20 +768,11 @@ angular.module('IMS8Alert.controllers', [])
     };
 })
 
-.controller('HomeCtrl', function ($scope, $rootScope, $ionicModal, $state, iAdminServiceClient, $ionicLoading, $ionicNavBarDelegate) {
-    function onBackKeyDown(e) {
-        e.preventDefault();
-        navigator.notification.confirm("Are you sure you want to exit ?", onConfirm, "Confirmation", "Yes,No");
-        // Prompt the user with the choice
-    }
-
-    function onConfirm(button) {
-        if (button == 2) {//If User selected No, then we just do nothing
-            return;
-        } else {
-            navigator.app.exitApp();// Otherwise we quit the app.
-        }
-    }
+.controller('HomeCtrl', function ($scope, $rootScope, $ionicModal, $state, iAdminServiceClient, $ionicLoading, $ionicNavBarDelegate, $ionicPlatform) {
+    $ionicPlatform.onHardwareBackButton(function () {
+        e.stopPropagation();
+        alert('you sure you want to exit?');
+    });
     getCustomers();
     if ($rootScope.CustomerName) {
         $scope.selectedcstomer = $rootScope.CustomerName;
