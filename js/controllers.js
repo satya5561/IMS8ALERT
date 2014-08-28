184cfe4a2fd8d2a0b41f92f630a904497fd5f626
satya5561 <satya5561@gmail.com>
2014-08-28 17:27:06 +0530

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -925,6 +925,11 @@ angular.module('IMS8Alert.controllers', [])
 .controller('AccountCtrl', function ($scope) {
 })
 .controller('MainController', function ($rootScope, $scope, $location) {
+    document.addEventListener("menubutton", onMenuKeyDown, false);
+
+function onMenuKeyDown() {
+alert("hello iAlert");
+};
     $scope.isSpecificPage = function () {
         var path;
         return path = $location.path(), _.contains(["/404", "/login", "/signin", "/"], path)
