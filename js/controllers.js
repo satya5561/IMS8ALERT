808f6381ddda577ca6d278ec289c215744ea41ec
saifansari2 <saif.ansari2@gmail.com>
2014-09-08 15:21:00 +0530

    For notification of connectivity

--- js/controllers.js
+++ js/controllers.js
@@ -1022,6 +1022,31 @@ angular.module('IMS8Alert.controllers', [])
     }
     else
         $rootScope.platform = "";
+        
+         setTimeout(function () {
+        var networkState = navigator.connection.type;
+        var states = {};
+        states[Connection.UNKNOWN] = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI] = 'WiFi connection';
+        states[Connection.CELL_2G] = 'Cell 2G connection';
+        states[Connection.CELL_3G] = 'Cell 3G connection';
+        states[Connection.CELL_4G] = 'Cell 4G connection';
+        states[Connection.CELL] = 'Cell generic connection';
+        states[Connection.NONE] = 'No network connection';
+        //alert('Connection type: ' + states[networkState]);
+        if (states[networkState] == "No network connection" || states[networkState] == "undefined") {
+            alert('Connection type: ' + states[networkState]);
+            ionic.Platform.exitApp();
+            window.close();
+            if (navigator.app) {
+                navigator.app.exitApp();
+            } else if (navigator.device) {
+                navigator.device.exitApp();
+            }
+        }     
+
+    }, 5000);
 
 })
 .controller('LoginCtrl', function ($scope, $state, iAdminServiceClient, $window, $ionicPopup, $ionicLoading, $ionicPlatform, $cordovaCamera, $cordovaNetwork) {
