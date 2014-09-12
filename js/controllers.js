6d0924beb9fa07aa04720f3ad240c35532575994
saifansari2 <saif.ansari2@gmail.com>
2014-09-12 12:35:40 +0530

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -1102,7 +1102,7 @@ function onBackButtonPress()   {
             }
         };
     $scope.showAlert1=function() {
-        alert('showAlert1 is calling');
+        $scope.count = 0;
         var alertPopup = $ionicPopup.alert({
             title: 'Internet Connection',
             template: 'Internet is not available. Please Enable Mobile Data or Wifi !'
